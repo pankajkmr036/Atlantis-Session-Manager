25329a089668478ec0f2f95a703ea729ef335575
Pankaj Kumar <pankajk@orowealth.com>
2021-10-15 12:29:39 +0530

    config: project directory setup
    1. added separate Landing Screen Component

--- App/index.js
+++ App/index.js
@@ -1,28 +1,8 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, View, StyleSheet} from 'react-native';
+import LandingScreen from './screens/LandingScreen.js';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.sectionContainer}>
-          <Text style={styles.sectionTitle}>Session manager</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <LandingScreen />;
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    alignSelf: 'center',
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-});
 
 export default App;
---
+++ App/screens/LandingScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, Text, View, StyleSheet} from 'react-native';
+
+const LandingScreen = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Session manager</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    alignSelf: 'center',
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default LandingScreen;
